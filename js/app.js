c09b695689d94d76f55cc9e2aab74ebbfa1a33ba
JMax <max@quamm.it>
2018-09-24 14:53:54 +0200

    Modifica codice js

--- js/app.js
+++ js/app.js
@@ -1,81 +1,102 @@
-// CREO LE VARIABILI LISTA / AGGIUNGI / RIMUOVI
-
-var lista = document.getElementById('list'),
-    remAll = document.getElementById('remAll'),
-    addtodo = document.getElementById('addtodo')
-
-// CHIAMO FUNZIONE AL CLICK DEL BOTTONE ADD
-addtodo.onclick = () => {
-    console.log("Hai cliccato il bottone "+addtodo.id);
-    addItem(lista);
+// VARIABILI STATO
+var stato = {
+	attivo: [],
+	completato: []
 };
 
-// AGGIUNGO TRIGGER ENTER
-var input = document.getElementById("instodo");
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      document.getElementById("addtodo").click();
-    }
+// LOCAL STORAGE GET
+var data = localStorage.getItem('data');
+if (data) {
+	stato = JSON.parse(data);
+}
+
+window.addEventListener('load', function handle() {
+	'use strict';
+	var listaAttiva = document.querySelector('ul.lista-attiva'),
+	    listaCompletata = document.querySelector('ul.lista-completata'),
+	    BloccoCompletato = document.querySelector('.blocco-completato'),
+	    input = document.querySelector('#instodo'),
+	    button = document.querySelector('button');
+
+	function aggiorna() {
+		var i,
+		    j,
+		    el;
+		listaAttiva.innerHTML = '';
+		listaCompletata.innerHTML = '';
+		stato.attivo.sort();
+		stato.completato.sort();
+
+		for (i = 0; i < stato.attivo.length; i++) {
+			el = document.createElement('li');
+			el.innerHTML = stato.attivo[i];
+			listaAttiva.appendChild(el);
+		}
+
+		for (j = 0; j < stato.completato.length; j++) {
+			el = document.createElement('li');
+			el.innerHTML = stato.completato[j];
+			listaCompletata.appendChild(el);
+		}
+	}
+
+	aggiorna();
+
+	listaAttiva.addEventListener('click', function(e) {
+		var a,
+            target = e.target;
+            
+		target.classList.add('completato');
+
+		for (a = 0; a < stato.attivo.length; a++) {
+			if (target.innerHTML === stato.attivo[a]) {
+				stato.completato.splice(1, 0, stato.attivo[a]);
+				stato.attivo.splice(a, 1);
+				localStorage.setItem('data', JSON.stringify(stato));
+                console.log(stato);
+                aggiorna();
+			}
+		}
+
+	});
+
+	button.addEventListener('click', function handleClick() {
+		var text = input.value;
+		if (text !== '') {
+			stato.attivo.push(text);
+			aggiorna();
+            localStorage.setItem('data', JSON.stringify(stato));
+            console.log(stato);
+			input.value = '';
+		}
+	});
+
+	input.addEventListener('keydown', function handleKeydown(e) {
+		var text = input.value;
+
+		if (e.keyCode === 13) {
+			if (text !== '') {
+				stato.attivo.push(text);
+                localStorage.setItem('data', JSON.stringify(stato));
+                console.log(stato);
+				aggiorna();
+				input.value = '';
+			}
+		}
+	});
+	
+	BloccoCompletato.addEventListener('click', function() {
+		listaCompletata.classList.toggle('invisible');
+		if (listaCompletata.classList.contains('fadein')) {
+			listaCompletata.classList.add('fadeout');
+			listaCompletata.classList.remove('fadein');
+		} else {
+			listaCompletata.classList.add('fadein');
+			listaCompletata.classList.remove('fadeout');
+		}
+    });
+
+   
+
 });
-
-// CHIAMO FUNZIONE AL CLICK DEL BOTTONE REMOVE
-remAll.onclick = () => {
-    console.log("Hai cliccato il bottone "+remAll.id)
-};
-
-// FUNZIONE AGGIUNGI ITEM ALLA LISTA
-function addItem(targetItem) {
-    // CREO LE VARIABILI
-    var testoItem = document.getElementById('instodo').value,
-        li = document.createElement('li'),
-        content = document.createTextNode(testoItem + ' '),
-        remBut = document.createElement('button');
-
-    // RIMUOVO IL TESTO DALL'INPUT TO-DO
-    document.getElementById('instodo').value = '';
-
-    // APPENDO IN NUOVO ELEMENTO TESTO ALLA LI
-    li.appendChild(content);
-    li.appendChild(remBut);
-    
-    
-    // CONTROLLO CHE L'INPUT NON SIA VUOTO
-    if (testoItem.split(' ').join('').length === 0) {
-        alert('non hai compilato il campo');
-        console.log('non hai compilato il campo');
-        return false;
-    } 
-
-
-    // AGGIUNTA DEL PULSTANTE ELIMINA SU OGNI ITEM
-    remBut.className = 'elimina';
-    remBut.innerHTML = ' Elimina';
-    remBut.setAttribute('onclick', 'elimina(this);');
-
-       
-
-    // APPENDO LA LI ALLA LISTA
-    targetItem.appendChild(li);
-    localStorage.setItem('todoList',list.innerHTML );
-    //console.log(localStorage);
-    
-
-}
-    
-// FUNZIONE PER ELIMINARE ITEM DALLA LISTA
-function elimina(item) {
-    var parent = item.parentElement;
-    parent.parentElement.removeChild(parent);
-    localStorage.setItem('todoList',list.innerHTML );
-}
-
-list.innerHTML = localStorage.getItem('todoList');
-
-// RIMUOVO TUTTI GLI ELEMENTI DELLA LISTA
-remAll.onclick = function() {
-    lista.innerHTML = '';
-    localStorage.setItem('todoList',list.innerHTML );
-};
-
 
